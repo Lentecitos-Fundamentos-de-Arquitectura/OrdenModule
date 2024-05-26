39df21a5fb8d3e93d9c2346a6be4036a5a1311b8
Willy Valentin Ricaldi <67374479+w1llyVR@users.noreply.github.com>
2024-05-26 18:00:41 -0500

    Delete src directory

--- src/main/java/com/lentecitos/design/design/design/domain/persistence/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lentecitos.design.design.design.domain.persistence;
-
-import com.lentecitos.design.design.design.domain.model.entities.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ColorRepository extends JpaRepository<Color, Integer> {
-}
--- src/main/java/com/lentecitos/design/design/design/domain/persistence/PatasAnteojosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lentecitos.design.design.design.domain.persistence;
-
-import com.lentecitos.design.design.design.domain.model.entities.PatasAnteojos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatasAnteojosRepository extends JpaRepository<PatasAnteojos, Integer> {
-}
