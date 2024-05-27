d0a5109ed1ab1b21aad58223efe258ddcaa2afb4
Alfredo Farro <48871201+AlfredoFarro@users.noreply.github.com>
2024-05-26 20:45:22 -0500

    Update AnteojosOrden.java
    Updating Fix

--- src/main/java/com/upc/order/design/domain/model/AnteojosOrden.java
+++ src/main/java/com/upc/order/design/domain/model/AnteojosOrden.java
@@ -1,1 +1,32 @@
+package com.upc.order.design.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "anteojos_orden")
+public class AnteojosOrden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "codigo_diseno_anteojos")
+    private Long codigoDisenoAnteojos;
+
+    @Column(name = "codigo_orden")
+    private Long codigoOrden;
+
+    @Column(name = "codigo_descuento")
+    private Long codigoDescuento;
+
+    @Column(name = "precio_base")
+    private double precioBase;
+
+    @Column(name = "precio_final")
+    private double precioFinal;
+}
 
