5c14e0c55e44b3c993170049c09aa07e22aaf228
OnofreR <104178574+OnofreR@users.noreply.github.com>
2024-05-26 21:28:49 -0500

    Update Descuento:  getters and setters

--- src/main/java/com/upc/order/design/domain/model/Descuento.java
+++ src/main/java/com/upc/order/design/domain/model/Descuento.java
@@ -12,4 +12,19 @@ import java.util.Date;
 @Table(name = "descuento")
 public class Descuento {
 
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "nombre", length = 250)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 250)
+    private String descripcion;
+
+    @Column(name = "caducidad")
+    private Date caducidad;
+
+    @Column(name = "porcentaje_descuento")
+    private double porcentajeDescuento;
+}
