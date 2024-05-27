e64a8c1ec24e49b7db557449955fe9d56a145873
OnofreR <104178574+OnofreR@users.noreply.github.com>
2024-05-26 21:36:22 -0500

    Update Orden: getters and setters

--- src/main/java/com/upc/order/design/domain/model/Orden.java
+++ src/main/java/com/upc/order/design/domain/model/Orden.java
@@ -1,1 +1,29 @@
-a
+package com.upc.order.design.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orden")
+public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "codigo_usuario")
+    private Long codigoUsuario;
+
+    @Column(name = "precio_total")
+    private double precioTotal;
+
+    @Column(name = "direccion_reparto")
+    private String direccionReparto;
+
+    @Column(name = "codigo_estado_orden")
+    private Long codigoEstadoOrden;
+
+}
