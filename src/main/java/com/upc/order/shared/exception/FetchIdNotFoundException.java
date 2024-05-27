43f44aa6aa38b100dcb701589cb3ee3311d2709c
The Editor <68383325+Marpellic@users.noreply.github.com>
2024-05-26 21:05:53 -0500

    FEAT: Implemented InternalServerErrorException

--- src/main/java/com/upc/order/shared/exception/FetchIdNotFoundException.java
+++ src/main/java/com/upc/order/shared/exception/FetchIdNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class FetchIdNotFoundException extends RuntimeException{
 public FetchIdNotFoundException(String resourceName, Integer resourceId) {
-        super(String.format("%s with id %d not found.", resourceName, resourceId))
+        super(String.format("%s with id %d not found.", resourceName, resourceId));
         StackTraceElement[] mio = {};
         this.setStackTrace(mio);
     }
